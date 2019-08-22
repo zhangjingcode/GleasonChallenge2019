6e587c9fb06008849a46aaaff38bffb510bbc08c
zhangjingmri <798582238@qq.com>
2019-08-23 00:48:25 +0800

    Seperated annotation image

--- ImageDraw.py
+++ ImageDraw.py
@@ -3,6 +3,10 @@ import os
 import re
 import numpy as np
 
+from collections import Counter
+from skimage import measure,draw
+
+import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 
@@ -37,6 +41,15 @@ def ReadLabelingImg(annotation_img_path):
 
     return annotation_img_array, pathologist_num
 
+def SeperateLabel(annotation_img_array):
+
+    seperate_label_array_dict = {}
+    for voxel_threshold in [1,3,4,5,6]:
+        if voxel_threshold in annotation_img_array:
+            sub_annotation_img_array = np.zeros(annotation_img_array.shape)
+            sub_annotation_img_array[np.where(annotation_img_array == voxel_threshold)] = voxel_threshold
+            seperate_label_array_dict[voxel_threshold] = sub_annotation_img_array
+    return seperate_label_array_dict
 
 
 def MarginPlot(core_img_path, annotation_img_path):
@@ -44,23 +57,43 @@ def MarginPlot(core_img_path, annotation_img_path):
     Draw the margin of annotation image in core imag path
     :param core_img_path: image file path of core pathology image
     :param annotation_img_path: image file path of pathologist annotation img
-    :return: ax
+    :return:
     '''
     core_img_array, case_name = ReadCoreImg(core_img_path)
     annotation_img_array, pathologist_num = ReadLabelingImg(annotation_img_path)
+    # if annotation_img_array[:,:,0].all() == annotation_img_array[:,:,1].all() and \
+    #     annotation_img_array[:, :, 0].all() == annotation_img_array[:, :, 2].all():
+    #     print('all same')
+
+
 
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 
-    #show img
+    # show img
+
     ax.imshow(core_img_array)
 
-    ## draw the contour of annotation image
-    cs = ax.contour(annotation_img_array[:,:,0], levels=[1, 3, 4, 5, 6],
-                cmap="Accent", linewidths=3)
-    fig.colorbar(cs, ax=ax,extendfrac=False)
+    # set color
+
+    color_map = {1:'lawngreen', 3:'darkorange', 4:'blue', 5:'red', 6:'black'}
+    seperate_label_array_dict = SeperateLabel(annotation_img_array[:,:,0])
+
+    # show contour in different voxel threshold
+
+    for voxel_threshold in [1, 3, 4, 5, 6]:
+        if voxel_threshold in seperate_label_array_dict.keys():
+            seperate_label_array = seperate_label_array_dict[voxel_threshold]
+            ax.contour(seperate_label_array, colors=color_map[voxel_threshold])
+            ax.plot(0, 0, '-', label=voxel_threshold, color=color_map[voxel_threshold])
 
 
-    ax.set_title(case_name+' by '+pathologist_num)
+
+    # cs = ax.contour(annotation_img_array[:,:,0], levels=[1, 3, 4, 5, 6],
+    #             cmap="Accent", linewidths=5)
+    # fig.colorbar(cs, ax=ax,extendfrac=False)
+
+    ax.legend()
+    ax.set_title(case_name+' in Maps of '+pathologist_num)
     ax.set_xticks([])
     ax.set_yticks([])
     plt.show()
