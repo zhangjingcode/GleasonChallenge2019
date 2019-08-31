92a7dd0cb10fb3e1a22b15268b9ab2af5cc2e6f5
zhangjingcode <40156485+zhangjingcode@users.noreply.github.com>
2019-08-31 14:39:49 +0800

    del

--- CustomerPath.py
+++
@@ -1,3 +0,0 @@
-core_img_path = r'W:\MRIData\OpenData\Gleason2019\Train Imgs\slide006_core110.jpg'
-annotation_img_path = r'W:\MRIData\OpenData\Gleason2019\Maps1_T\slide006_core110_classimg_nonconvex.png'
-store_path = r'C:\Users\zj\Desktop\Gleason'
