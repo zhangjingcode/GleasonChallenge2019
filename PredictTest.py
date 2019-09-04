93d9716b99e5a48e7507fcb51a9698ac9f1fe6d9
zhangjingmri <798582238@qq.com>
2019-09-04 23:31:57 +0800

    Update

--- PredictTest.py
+++ PredictTest.py
@@ -1,35 +1,16 @@
 # test
-<<<<<<< Updated upstream:PredictTest.py
 import os
 import numpy as np
 import h5py
-=======
-import tensorflow as tf
-config = tf.ConfigProto(log_device_placement=True)
-config.gpu_options.per_process_gpu_memory_fraction=0.3 # don't hog all vRAM
-config.operation_timeout_in_ms=15000   # terminate on long hangs
-sess = tf.InteractiveSession("", config=config)
->>>>>>> Stashed changes:Test.py
 
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 
-<<<<<<< Updated upstream:PredictTest.py
 from CustomerPath import model_path, testing_folder, save_path
-=======
-<<<<<<< Updated upstream
-model_path = r'D:\Gleason2019\TrainValidationTest_256\model_binary_entropy'
-from MeDIT.SaveAndLoad import LoadModel
-=======
-
-model_path = r'D:\Gleason2019\TrainValidationTest_256\model_categorical_crossentropy'
-from MeDIT.CNNModel.SaveAndLoad import LoadModel,LoadHistory
->>>>>>> Stashed changes
-model = LoadModel(model_path, 'best_weights.h5', is_show_summary=True)
->>>>>>> Stashed changes:Test.py
+from CNNModel.Training.Generate import ImageInImageOut2DTest
 
 
-input_shape = [240, 240, 3]
+input_shape = [496, 496, 3]
 batch_size = 4
 
 
@@ -39,21 +20,17 @@ def LoadTest(testing_folder, input_shape):
     return input_list, output_list, case_list
 
 
-<<<<<<< Updated upstream:PredictTest.py
+input_list, output_list, case_list = LoadTest(testing_folder, input_shape)
+
+
 def SavePredict(model_path, input_list, save_path, batch_size):
     from CNNModel.Utility.SaveAndLoad import LoadModel
     model = LoadModel(model_path, 'best_weights.h5', is_show_summary=True)
     pred = model.predict(input_list, batch_size=batch_size)
     np.save(os.path.join(save_path, 'prediction_test.npy'), pred)
-=======
-def SavePredArray(pred, save_path=''):
-    import numpy as np
-    import os
-    for case_num in range(pred.shape[0]):
-        pred_array = pred[case_num, :,:,:]
-        np.save(os.path.join(save_path, str(case_num)+'.npy'), pred_array)
 
->>>>>>> Stashed changes:Test.py
+
+# SavePredict(model_path, input_list, save_path, batch_size)
 
 
 def ShowPred(output_list, save_path=''):
@@ -131,27 +108,22 @@ def ShowPred(output_list, save_path=''):
             plt.savefig(sub_save_path)
             plt.close()
         # plt.show()
-<<<<<<< Updated upstream:PredictTest.py
 
-# ShowPred(output_list, pred, save_path=r'D:\Gleason2019\TrainValidationTest_256\model_binary_entropy\PredShowTest')
-=======
-# LoadHistory(model_path, is_show=True)
-# show_train_history(history, 'loss', 'val_loss')
-# ShowPred(output_list, pred, save_path=r'D:\Gleason2019\TrainValidationTest_256\model_categorical_crossentropy\PredShow')
->>>>>>> Stashed changes:Test.py
+
+# ShowPred(output_list, save_path=save_path)
 # plt.savefig(os.path.join(os.path.split(model_path)[0], 'ROC.png'))
-SavePredArray(pred, r'D:\Gleason2019\TrainValidationTest_256\TrainValidationTest_256\TestPred')
+
 
 def SavePredH5(input, output, save_path=''):
     pred = np.load(os.path.join(save_path, 'prediction_test.npy'))
     for case_num in range(output.shape[0]):
-        label_name = 'date' + str(case_num) + '.h5'
-        data_path = os.path.join(save_path, 'LabelH5', label_name)
+        label_name = 'data' + str(case_num) + '.h5'
+        data_path = os.path.join(save_path, 'PredictH5', label_name)
         with h5py.File(data_path, 'w') as f:
             f['input_0'] = input[case_num, :]
             f['output_0'] = output[case_num, :]
             f['predict_0'] = pred[case_num, :]
-
+# SavePredH5(input_list, output_list, save_path)
 
 def MergeOnePred(one_label):
     merged_one_label = np.zeros(shape=(one_label.shape[0], one_label.shape[1]), dtype=np.float32)
@@ -223,14 +195,15 @@ def MergeLabel(save_path, show_pixls=False):
             print("name:{},     pred pixls:{}".format(case_list[case_num], pred_pixls))
 
 
-MergeLabel(save_path)
+# MergeLabel(save_path)
 
 
-# data_path = r'D:\data\GleasonChallenge2019\Merged_512\Test\slide006_core114.h5'
-# with h5py.File(data_path, 'r') as file:
-#     image = np.asarray(file['input_0'], dtype=np.float32)
-#     label = np.asarray(file['output_0'], dtype=np.uint8)
-#     plt.contour(label[:, :, 0], colors='r')
-#     plt.imshow(image[:, :, 0], cmap='gray')
-#     plt.show()
+data_path = r'D:\data\GleasonChallenge2019\Merged_512\model\PredictH5\data10.h5'
+with h5py.File(data_path, 'r') as file:
+    image = np.asarray(file['input_0'], dtype=np.float32)
+    label = np.asarray(file['output_0'], dtype=np.uint8)
+    predict = np.asarray(file['predict_0'], dtype=np.float32)
+    plt.contour(label[:, :, 0], colors='r')
+    plt.imshow(predict[:, :, 0], cmap='gray')
+    plt.show()
 
