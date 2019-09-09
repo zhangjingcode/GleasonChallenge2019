8494844a16be9164a0e245e1c3aa9933358d23fb
cherishzyh <anewdaycherish@gmail.com>
2019-09-09 15:41:25 +0800

    TransferLearning

--- TransferLearning.py
+++ TransferLearning.py
@@ -1,8 +1,16 @@
 from UNet import UNet
 
+input_shape = ()
 
-def TestModel():
-    model = UNet((240, 240, 3))
+
+def TestModel(input_shape):
+    model = UNet(input_shape)
     model.layers[-1].name = 'pred'
     model.load_weights(r'D:\data\TZ_ROI\Model\best_weights.h5', by_name=True)
     model.summary()
+
+
+# def TransLearning(input_shape):
+#     model = UNet(input_shape)
+#     new_model = model.get_layer(index=-1)
+#     new_model
