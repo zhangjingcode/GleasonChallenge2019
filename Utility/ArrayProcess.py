6388ea85053819931b71a26e400d15682ba07de1
cherishzyh <anewdaycherish@gmail.com>
2019-09-13 23:53:29 +0800

    Enlargement

--- Utility/ArrayProcess.py
+++ Utility/ArrayProcess.py
@@ -27,11 +27,13 @@ def SeparateLabel(annotation_img_array):
             seperate_label_array_dict[specific_label] = sub_annotation_img_array
     return seperate_label_array_dict
 
+
 def EnlargementPatch(patch_array, enlarged_size, center_point=[-1, -1], is_shift=True):
+
     '''
     Extract patch from a 2D image.
-    :param enlarged_size: the 2D numpy array
-    :param patch_array: the size of the 2D patch
+    :param patch_array: the size of the 2D patch, (448, 448)
+    :param enlarged_size: the 2D numpy array, (5120//10, 4608//10)
     :param center_point: the center position of the patch
     :param is_shift: If the patch is too close to the edge of the image, is it allowed to shift the patch in order to
     ensure that extracting the patch close to the edge. Default is True.
@@ -40,27 +42,28 @@ def EnlargementPatch(patch_array, enlarged_size, center_point=[-1, -1], is_shift
 
     enlarged_array = np.zeros(enlarged_size)
 
-    patch_array = np.asarray(patch_array)
-    if patch_array.shape == () or patch_array.shape == (1,):
-        patch_array = np.array([patch_array[0], patch_array[0]])
+    # if patch_array.shape == () or patch_array.shape == (1,):
+    #     patch_array = np.array([patch_array[0], patch_array[1]])
 
-    image_row, image_col = np.shape(enlarged_size)
-    if patch_array[0] // 2 == image_row - (patch_array[0] // 2):
-        catch_x_index = [patch_array[0] // 2]
+    image_row = enlarged_size[0]
+    image_col = enlarged_size[1]
+
+    if patch_array.shape[0] // 2 == image_row - (patch_array.shape[0] // 2):
+        catch_x_index = [patch_array.shape[0] // 2]
     else:
-        catch_x_index = np.arange(patch_array[0] // 2, image_row - (patch_array[0] // 2))
-    if patch_array[1] // 2 == image_col - (patch_array[1] // 2):
-        catch_y_index = [patch_array[1] // 2]
+        catch_x_index = np.arange(patch_array.shape[0] // 2, image_row - (patch_array.shape[0] // 2))
+    if patch_array.shape[1] // 2 == image_col - (patch_array.shape[1] // 2):
+        catch_y_index = [patch_array.shape[1] // 2]
     else:
-        catch_y_index = np.arange(patch_array[1] // 2, image_col - (patch_array[1] // 2))
+        catch_y_index = np.arange(patch_array.shape[1] // 2, image_col - (patch_array.shape[1] // 2))
 
     if center_point == [-1, -1]:
         center_point[0] = image_row // 2
         center_point[1] = image_col // 2
 
-    if patch_array[0] > image_row or patch_array[1] > image_col:
-        print('The patch_size is larger than image shape')
-        return np.array([])
+    # if patch_array.shape[0] > image_row or patch_array.shape[1] < image_col:
+    #     print('The patch_size is larger than image shape')
+    #     return np.array([])
 
     if center_point[0] < catch_x_index[0]:
         if is_shift:
@@ -87,8 +90,51 @@ def EnlargementPatch(patch_array, enlarged_size, center_point=[-1, -1], is_shift
             print('The center point is too close to the positive y-axis')
             return []
 
-    patch_row_index = [center_point[0] - patch_array[0] // 2, center_point[0] + patch_array[0] - patch_array[0] // 2]
-    patch_col_index = [center_point[1] - patch_array[1] // 2, center_point[1] + patch_array[1] - patch_array[1] // 2]
+    patch_row_index = [center_point[0] - patch_array.shape[0] // 2, center_point[0] + patch_array.shape[0] - patch_array.shape[0] // 2]
+    patch_col_index = [center_point[1] - patch_array.shape[1] // 2, center_point[1] + patch_array.shape[1] - patch_array.shape[1] // 2]
 
     enlarged_array[patch_row_index[0]:patch_row_index[1], patch_col_index[0]:patch_col_index[1]] = patch_array
     return enlarged_array, [patch_row_index, patch_col_index]
+
+def CheckEnlargment(h5_file_path):
+    import h5py
+    with h5py.File(h5_file_path, 'r') as target_h5_file:
+
+        core_img_array = target_h5_file['input_0'].value
+        # annotation_array = target_h5_file['output_0'].value
+        predict_img_array = target_h5_file['predict_0'].value
+
+    EnlargementPatch(predict_img_array[..., 0], (512, 460))
+
+def TestEnlarge():
+    from GleasonChallenge2019.Model.Generator import ImageIn2DTest
+    from GleasonChallenge2019.Model.Generator import GetH5Data
+
+    import matplotlib.pyplot as plt
+
+    input_shape = [448, 448, 3]
+
+    input_list, shape_list, case_list = ImageIn2DTest(r'D:\ZYH\Data\GleasonChallenge2019\Test_h5\Test', input_shape=input_shape)
+    predict_list, _ = GetH5Data(r'D:\ZYH\Data\GleasonChallenge2019\Test_h5\model\ProjectPredictH5')
+
+    for case_num in range(len(case_list)):
+        patch = (shape_list[case_num][0]//10, shape_list[case_num][1]//10)
+        predict = predict_list[case_num]
+
+        for index in range(predict.shape[-1]):
+            enlarged_array, list = EnlargementPatch(predict[:, :, index], patch)
+
+            print(list)
+            plt.imshow(enlarged_array)
+            plt.axis('off')
+            plt.show()
+
+
+def main():
+    h5_path = r'D:\ZYH\Data\GleasonChallenge2019\Test_h5\model\ProjectPredictH5\slide001_core015_.h5'
+    CheckEnlargment(h5_path)
+
+if __name__ == '__main__':
+    main()
+# TestEnlarge()
+
